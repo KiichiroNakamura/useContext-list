87c5fe75edc0489345e7eb3d61e59a9d743ce205
KiichiroNakamura <47166584+KiichiroNakamura@users.noreply.github.com>
2021-09-27 15:40:14 +0900

    202109271540

--- src/components/FirstList.jsx
+++ src/components/FirstList.jsx
@@ -1,17 +1,22 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 
 //import { ListContext } from "../providers/ListProvider";
 
 export const FirstList = () => {
   const [listVal, setListVal] = useState(["aaaaa", "bbbb"]);
   // const [listData, setListData] = useContext(ListContext);
-
-  const onChangeText = (e) => {};
+  const addListItem = useRef(null);
+  const addItem = () => {
+    console.log(addListItem.current.value);
+    const newList = [addListItem.current.value, ...listVal];
+    setListVal(newList);
+  };
 
   return (
     <div>
-      <h1>Hello</h1>
-      <input text="text" onChange={onChangeText} />
+      <h1>List</h1>
+      <input text="text" ref={addListItem} />
+      <button onClick={addItem}>add</button>
       <ul>
         {listVal.map((list, index) => {
           return <li key={index}>{list}</li>;
