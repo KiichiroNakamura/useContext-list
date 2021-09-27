6996339a06439a0926059b825f345a37cc3e194c
KiichiroNakamura <47166584+KiichiroNakamura@users.noreply.github.com>
2021-09-27 15:50:55 +0900

    202109271550

--- src/components/FirstList.jsx
+++ src/components/FirstList.jsx
@@ -1,16 +1,21 @@
-import React, { useContext, useState, useRef } from "react";
+import React, { useContext, useState, useRef, useEffect } from "react";
 
-//import { ListContext } from "../providers/ListProvider";
+import { ListContext } from "../providers/ListProvider";
 
 export const FirstList = () => {
-  const [listVal, setListVal] = useState(["aaaaa", "bbbb"]);
-  // const [listData, setListData] = useContext(ListContext);
+  const [listData, setListData] = useContext(ListContext);
+  // const [listVal, setListVal] = useState(["aaaaa", "bbbb"]);
+  const [listVal, setListVal] = useState(listData);
+
   const addListItem = useRef(null);
   const addItem = () => {
     console.log(addListItem.current.value);
     const newList = [addListItem.current.value, ...listVal];
-    setListVal(newList);
+    setListData(newList);
   };
+  useEffect(() => {
+    setListVal(listData);
+  }, [listData]);
 
   return (
     <div>
